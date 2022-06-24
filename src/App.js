2a4094bd13cec93458d7603f8422984cb3c401e3
iamajayshirke <ajay.shirke@fugetroncorp.com>
2022-06-24 22:56:47 +0530

    uncomment changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Here We Go Git Tutorial</p>
-       <p>Git Logs: To Track All Commits</p>
-       <p>Git Status: To Track Modification 37632 And Staging Area</p>
-       {/* <p>Git Status: To Track Modification And Staging Area</p> */}
-       New Changes
+        <p>Here We Go Git Tutorial</p>
+        <p>Git Logs: To Track All Commits</p>
+        <p>Git Status: To Track Modification 37632 And Staging Area</p>
+        <p>Git Status: To Track Modification And Staging Area</p>
+        New Changes
       </header>
     </div>
   );
